17a901493f91221aa25ab3f6d3f35dda66dc7cce
evelynvic23 <evelynvic2310@gmail.com>
2024-09-25 10:04:31 -0300

    Docs Respondendo as Questões

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,17 @@ import java.util.Scanner;
 public class App {
         //Comentário
 
+//- Na classe Corpo_Humano inclua a linha c1.massa = "2"; logo depois de instanciar o objeto c1. Qual foi a ocorrência? Qual a conclusão sobre o que aconteceu?
+
+// Não consigo acessar
+
+// - Na classe Corpo_Humano altere a linha private float Massa para public float Massa; Qual foi a ocorrência? Qual a conclusão sobre o que aconteceu?
+
+// Daria erro
+
+// - Na classe Corpo_Humano altere a linha public setVolume (float volume) para private setVolume (float volume).Qual foi a ocorrência? Qual a conclusão sobre o que aconteceu?
+
+// Não conseguiria acessar
    
     public static void main(String[] args) throws Exception {
         
